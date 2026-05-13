8cc277e6318c942ee2e3befd4edd407f9b8fe175
William Alves <alves.williamsilva@gmail.com>
2026-05-13 17:33:01 +0200

    chore: setup husky and lint-staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <h1 className="text-6xl font-bold underline">Cinext</h1>
       </main>
     </div>
-  );
+  )
 }
